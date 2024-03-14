420349dfeac323f8d4b31f047d1a6b4fd2b6a550
alexantepa <antepa.alex04@gmail.com>
2024-03-14 15:06:10 +0000

    petrushin

---
+++ calllog.sql
@@ -0,0 +1,75 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: isu
+-- ------------------------------------------------------
+-- Server version	8.0.36-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `calllog`
+--
+
+DROP TABLE IF EXISTS `calllog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `calllog` (
+  `start` date DEFAULT NULL,
+  `end` date DEFAULT NULL,
+  `length` int DEFAULT NULL,
+  `outgoing_number` varchar(20) DEFAULT NULL,
+  `incoming_number` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calllog`
+--
+
+LOCK TABLES `calllog` WRITE;
+/*!40000 ALTER TABLE `calllog` DISABLE KEYS */;
+INSERT INTO `calllog` VALUES ('2024-03-14','2024-03-15',244,'+7125184503','+7188002590'),('2024-03-16','2024-03-14',180,'+7215479701','+7283189421'),('2024-03-18','2024-03-19',205,'+7412762497','+719805529'),('2024-03-19','2024-03-14',192,'+7463153565','+7398163870'),('2024-03-17','2024-03-18',78,'+7850260518','+7479003007'),('2024-03-18','2024-03-18',117,'+7587985928','+7775652720'),('2024-03-13','2024-03-14',264,'+7665976732','+7690069689'),('2024-03-16','2024-03-14',181,'+7435137825','+7369218241'),('2024-03-16','2024-03-17',107,'+7995984397','+7910009981'),('2024-03-16','2024-03-13',215,'+7338533030','+7544728505'),('2024-03-17','2024-03-19',122,'+7312048716','+7342157088'),('2024-03-18','2024-03-18',273,'+78368715','+7312716712'),('2024-03-16','2024-03-18',47,'+7516055246','+7113016307'),('2024-03-13','2024-03-18',204,'+7147928317','+7708930189'),('2024-03-13','2024-03-15',176,'+7792880859','+7209104559');
+/*!40000 ALTER TABLE `calllog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `numbers_table`
+--
+
+DROP TABLE IF EXISTS `numbers_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `numbers_table` (
+  `phone_number` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `numbers_table`
+--
+
+LOCK TABLES `numbers_table` WRITE;
+/*!40000 ALTER TABLE `numbers_table` DISABLE KEYS */;
+INSERT INTO `numbers_table` VALUES ('+1234567890'),('+1987654321'),('+1654321890'),('+1789054321');
+/*!40000 ALTER TABLE `numbers_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-13 11:15:00
